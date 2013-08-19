024f858781486872246beb13c60783745574181a
abuabdul <abuabdul86@hotmail.com>
2013-08-20 00:32:48 +0530

    initial castor exercise commit

---
+++ src/main/java/com/abuabdul/castor/exercise/App.java
@@ -0,0 +1,13 @@
+package com.abuabdul.castor.exercise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
